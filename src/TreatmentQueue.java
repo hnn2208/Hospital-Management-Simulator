9c0f607680b16116e3ad67e309e517e8ae74f78b
Hanan Taher <hanan.dahir10@gmail.com>
2025-12-16 23:28:39 +0300

    innit commit

--- src/TreatmentQueue.java
+++ src/TreatmentQueue.java
@@ -1,2 +1,64 @@
 public class TreatmentQueue {
+
+    private class Node {
+        TreatmentRequest data;
+        Node next;
+
+        Node(TreatmentRequest data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    private Node front;
+    private Node rear;
+    private int size;
+
+    public TreatmentQueue() {
+        front = null;
+        rear = null;
+        size = 0;
+    }
+    public void enqueue(TreatmentRequest request) {
+        Node newNode = new Node(request);
+
+        if (rear == null) {
+            front =  newNode;
+            rear = newNode;
+        } else{
+            rear.next = newNode;
+            rear = newNode;
+        }
+        size++;
+    }
+
+    public TreatmentRequest dequeue() {
+        front = front.next;
+
+        if (front == null) {
+            return null;
+        }
+
+        TreatmentRequest temp = front.data;
+
+        if(front == null ) {
+            rear = null;
+        }
+
+        size--;
+        return temp;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public void printQueue() {
+        Node current = front; 
+
+        while (current != null) {
+            System.out.println(
+                    "Patient ID: " + current.data.patientId + ", Priority: " + current.data.priority);
+            current = current.next;
+        }
+    }
 }
