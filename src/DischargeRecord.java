d2fac269cbd981b9ff85fe2108a457ff477753c3
Hanan Taher <hanan.dahir10@gmail.com>
2025-12-17 00:03:38 +0300

    innit commit

--- src/DischargeRecord.java
+++ src/DischargeRecord.java
@@ -1,14 +1,13 @@
 public class DischargeRecord {
-
     int patientId;
     long dischargeTime;
 
     public DischargeRecord(int patientId){
-        this.patientId;
+        this.patientId = patientId;
         this.dischargeTime = System.currentTimeMillis();
     }
 
-    override
+    @Override
     public String toString() {
         return "Discharge{patientId=" + patientId + ", time=" + dischargeTime +'}';
     }
