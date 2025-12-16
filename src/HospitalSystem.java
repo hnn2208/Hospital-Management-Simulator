5d7fc5688b26efca804e5a11d4a5e4e4696be597
Hanan Taher <hanan.dahir10@gmail.com>
2025-12-17 02:50:37 +0300

    innit commin

--- src/HospitalSystem.java
+++ src/HospitalSystem.java
@@ -4,10 +4,54 @@ public class HospitalSystem {
 
     PatientList patientList;
     TreatmentQueue normalQueue;
-    TreatmentQueue PriorityQueue;
-    DischargeStack discahrgeStack;
-    HashMap<Integer, Patient> PatientMap;
+    TreatmentQueue priorityQueue;
+    DischargeStack dischargeStack;
+    HashMap<Integer, Patient> patientMap;
 
-    Public Hospital
+    public HospitalSystem() {
+        patientList = new PatientList();
+        normalQueue = new TreatmentQueue();
+        priorityQueue = new TreatmentQueue();
+        dischargeStack = new DischargeStack();
+        patientMap = new HashMap<>();
+    }
 
+    public void addPatient(Patient p) {
+        patientList.addPatient(p);
+        patientMap.put(p.id, p);
+    }
+
+    public void addTreatmentRequest(TreatmentRequest request) {
+        if (request.priority) {
+            priorityQueue.enqueue(request);
+        } else {
+            normalQueue.enqueue(request);
+        }
+    }
+
+    public void processTreatment() {
+        TreatmentRequest request;
+
+        if(priorityQueue.size() > 0) {
+            request = priorityQueue.dequeue();
+        } else {
+            request = normalQueue.dequeue();
+        }
+        if (request != null) {
+            dischargeStack.push(new DischargeRecord(request.patientId));
+        }
+    }
+    public void printSystemState() {
+        System.out.println("Patients:");
+        patientList.printList();
+
+        System.out.println("\nNormal Queue;");
+        normalQueue.printQueue();
+
+        System.out.println("nPriority Queue:");
+        priorityQueue.printQueue();
+
+        System.out.println("\nDischarge Stack:");
+        dischargeStack.printStack();
+    }
 }
