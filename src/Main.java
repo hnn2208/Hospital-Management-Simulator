f3deceaa0916db35e6bd842d89df0c92d5f7ca93
Hanan Taher <hanan.dahir10@gmail.com>
2025-12-17 03:33:00 +0300

    innit commin

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
 
 public class Main {
+
+
     public static void main(String[] args) {
+        System.out.println("Program started");
+        HospitalSystem system = new HospitalSystem();
 
+        system.addPatient(new Patient(1, "Fatma",  5,20));
+        system.addPatient(new Patient(2, "Huda", 8, 10));
+        system.addPatient(new Patient(3, "Malik", 4, 30));
 
+        system.addTreatmentRequest(new TreatmentRequest(1, false));
+        system.addTreatmentRequest(new TreatmentRequest(2, false));
+        system.addTreatmentRequest(new TreatmentRequest(3, true));
     }
 }
+
