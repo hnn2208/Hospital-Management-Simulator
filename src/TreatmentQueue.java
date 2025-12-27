96050742457daf1d4959b01d84795e1c4bdc87af
Hanan Taher <hanan.dahir10@gmail.com>
2025-12-27 16:01:15 +0300

    final version of TreatmentQueue file

--- src/TreatmentQueue.java
+++ src/TreatmentQueue.java
@@ -1,8 +1,31 @@
 import java.util.LinkedList;
 
 public class TreatmentQueue {
+    //i am Using a LinkedList to handle Queue operations
     private LinkedList<TreatmentRequest> list = new LinkedList<>();
-    public void enqueue(TreatmentRequest r) { list.addLast(r); }
-    public TreatmentRequest dequeue() { return list.pollFirst(); }
-    public int size() { return list.size(); }
+
+    //  i made it for adding to the back of the line
+    public void enqueue(TreatmentRequest request) {
+        list.addLast(request);
+    }
+
+    // i made this for Remove from the front
+    public TreatmentRequest dequeue() {
+        if (list.isEmpty()) {
+            return null;
+        }
+        return list.removeFirst(); // this is FIFO: First-In, First-Out
+    }
+
+    public int size() {
+        return list.size();
+    }
+
+    // Printing the queue
+    public void printQueue() {
+        System.out.println("--- Waiting Queue ---");
+        for (TreatmentRequest req : list) {
+            System.out.println("Patient ID: " + req.patientId + " | Urgent: " + req.isUrgent);
+        }
+    }
 }
