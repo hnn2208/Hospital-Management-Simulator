c1c15704cf3e2f2cadb32c1b6b3fc36ca12076d0
Hanan Taher <hanan.dahir10@gmail.com>
2025-12-27 19:38:12 +0300

    final version of DischargeStack.java

--- src/DischargeStack.java
+++ src/DischargeStack.java
@@ -1,7 +1,32 @@
-import java.util.Stack;
+import java.util.LinkedList;
 
 public class DischargeStack {
-    private Stack<DischargeRecord> stack = new Stack<>();
-    public void push(DischargeRecord r) { stack.push(r); }
-    public int size() { return stack.size(); }
+    // I used a LinkedList to act as a Stack
+    private LinkedList<DischargeRecord> my_stack = new LinkedList<>();
+
+    //  also i write this  bc of adding a record to the top
+    public void push(DischargeRecord record) {
+        my_stack.addFirst(record);
+    }
+
+    //  this one will Remove the most recent record
+    public DischargeRecord pop() {
+        if (my_stack.isEmpty()) {
+            return null;
+        }
+        return my_stack.removeFirst();
+    }
+
+    // this will Look at the top without removing
+    public DischargeRecord peek() {
+        return my_stack.peekFirst();
+    }
+
+    // Printing for the test scenario
+    public void printStack() {
+        System.out.println("--- Most Recent Discharges ---");
+        for (DischargeRecord dr : my_stack) {
+            System.out.println("Patient ID: " + dr.patientId + " | Time: " + dr.dischargeTime);
+        }
+    }
 }
